658478b06044b1b6cd462e8711c0eebf6235398f
Peter Sandor <pepe1125@gmail.com>
2022-01-05 02:18:08 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-    <?php
-    $username = "root";
-    $pass = "SP1234SP";
-    $host = "sp.myddns.me";
-    $db_name = "kulcs";
-    $con = mysqli_connect ($host, $username, $pass);
-    $db = mysqli_select_db ( $con, $db_name );
-    ?>
